2b116e45b06ea185df1c0e0387d3bac9c82b204a
李俊 <827916100@qq.com>
2023-02-14 10:51:43 +0800

    仓管初始化

---
+++ admin/src/main/java/info/batcloud/wxc/admin/controller/warehouse/Controller.java
@@ -0,0 +1,17 @@
+package info.batcloud.wxc.admin.controller.warehouse;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+/**
+ * @ClassName: Controller
+ * @Description:
+ * @Author V
+ * @Date 14/2/2023
+ * @Version 1.0
+ */
+@RestController
+@RequestMapping("/api/warehouse/")
+public class Controller {
+
+}
---
+++ core/src/main/java/info/batcloud/wxc/core/warehouse/dao/RequireGoodsDao.java
@@ -0,0 +1,14 @@
+package info.batcloud.wxc.core.warehouse.dao;
+
+import org.apache.ibatis.annotations.Mapper;
+
+/**
+ * @ClassName: RequireGoodsDao
+ * @Description:
+ * @Author V
+ * @Date 14/2/2023
+ * @Version 1.0
+ */
+@Mapper
+public interface RequireGoodsDao {
+}
