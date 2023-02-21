a259fa3752cb8cae3c591a4493ccf80020dcf5be
qqb <781944869@qq.com>
2023-02-21 16:34:20 +0800

    000

--- merchant.api/src/main/java/info/batcloud/wxc/merchant/api/controller/warehouse/ReceiptController.java
+++ merchant.api/src/main/java/info/batcloud/wxc/merchant/api/controller/warehouse/ReceiptController.java
@@ -1,13 +1,18 @@
 package info.batcloud.wxc.merchant.api.controller.warehouse;
 
 import com.github.pagehelper.PageInfo;
+import info.batcloud.wxc.core.domain.BusinessResponse;
 import info.batcloud.wxc.core.entity.CommonResult;
 import info.batcloud.wxc.core.entity.CommonResultPage;
+import info.batcloud.wxc.core.helper.SecurityHelper;
 import info.batcloud.wxc.core.service.warehouse.service.PurchaseOrderService;
 import info.batcloud.wxc.core.service.warehouse.service.ReceiptService;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
+import java.util.HashMap;
+import java.util.Map;
 
 /**
  * @ClassName: ReceiptController
@@ -18,6 +23,7 @@ import javax.annotation.Resource;
  */
 @RestController
 @RequestMapping("/api/receipt")
+@PreAuthorize("hasRole('STORE_USER')")
 public class ReceiptController {
     @Resource
     private ReceiptService receiptService;
@@ -54,19 +60,21 @@ public class ReceiptController {
         purchaseOrderService.updatePurchaseGoodsToApp(id,actualArrivalNum,actualArrivalSumprice);
         return new CommonResult("操作成功");
     }
-    
+
     /**
      *
      * describe 获取门店收货单列表
      * @author V
      * @date 20/2/2023 下午2:51
-     * @param 
-     * @return 
+     * @param
+     * @return
      */
     @GetMapping("/getReceiptByStoreId")
-    public CommonResultPage getReceiptByStoreId(@RequestParam("storeId") Integer storeId,@RequestParam("page") Integer page){
-        PageInfo receiptByStoreId = receiptService.getReceiptByStoreId(storeId, page);
-        return new CommonResultPage(receiptByStoreId,receiptByStoreId.getPageNum(),receiptByStoreId.getTotal(),receiptByStoreId.getPageSize(),receiptByStoreId.getNextPage(),receiptByStoreId.isHasNextPage());
+    public Object getReceiptByStoreId(@RequestParam("page") Integer page){
+        PageInfo receiptByStoreId = receiptService.getReceiptByStoreId(SecurityHelper.loginStoreUserId().intValue(), page);
+        Map<String, Object> map = new HashMap<>(1);
+        map.put("data",new CommonResultPage(receiptByStoreId,receiptByStoreId.getPageNum(),receiptByStoreId.getTotal(),receiptByStoreId.getPageSize(),receiptByStoreId.getNextPage(),receiptByStoreId.isHasNextPage()));
+        return BusinessResponse.ok(map);
     }
 
     /**
@@ -78,9 +86,11 @@ public class ReceiptController {
      * @return
      */
     @GetMapping("/getReceiptById")
-    public CommonResultPage getReceiptById(@RequestParam("id") Integer id,@RequestParam("page") Integer page){
+    public Object getReceiptById(@RequestParam("id") Integer id,@RequestParam("page") Integer page){
         PageInfo receiptGoodsById = receiptService.getReceiptGoodsById(id, page);
-        return new CommonResultPage(receiptGoodsById,receiptGoodsById.getPageNum(),receiptGoodsById.getTotal(),receiptGoodsById.getPageSize(),receiptGoodsById.getNextPage(),receiptGoodsById.isHasNextPage());
+        Map<String, Object> map = new HashMap<>(1);
+        map.put("data",new CommonResultPage(receiptGoodsById,receiptGoodsById.getPageNum(),receiptGoodsById.getTotal(),receiptGoodsById.getPageSize(),receiptGoodsById.getNextPage(),receiptGoodsById.isHasNextPage()));
+        return BusinessResponse.ok(map);
     }
 
 
